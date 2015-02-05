9061b42c0c16a03985a0da1a7cce137dc031c567
gotdairyya <deryaakbaba-locus@Deryas-MacBook-Air.local>
2015-02-04 20:33:15 -0500

    my first website

---
+++ script.js
@@ -0,0 +1,8 @@
+
+var myFreeTime = ["watercolor","playing basketball","make cakes","hunt for unicorns"];
+
+function printValue(value) {
+	console.log("I like to: ", value);
+}
+
+myFreeTime.forEach(printValue);
